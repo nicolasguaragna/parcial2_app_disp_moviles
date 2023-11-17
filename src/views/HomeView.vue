b23319ca8dc16102360c904d575716a5d2f5d34d
nicolasguaragna <132408707+nicolasguaragna@users.noreply.github.com>
2023-11-17 19:22:26 -0300

    bienv

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Bienvenidos a Gastox"/>
+    <HelloWorld msg="Bienvenidos a Gasto"/>
   </div>
 </template>
 
